6207c2419f729c98bb5ea5945dd0282cd5fde775
Diego Cruz Aguilar <dcrag@pm.me>
2022-10-16 22:00:22 -0500

    Fix errors

--- lua/betterTerm.lua
+++ lua/betterTerm.lua
@@ -23,106 +23,126 @@ M.setup = function(user_options)
   })
 end
 
-local function getIndex(num)
+--- Get table index for id term
+---@param num number
+---@return string?
+local function get_term_key(num)
+  if num == nil then return nil end
   return string.format(options.prefix .. "%d", num)
 end
 
---- Save characteristic of new term
----@param num number of terminal
-local function newTermcConfig(num)
-  local bufname = getIndex(num)
+--- Save information of new term
+---@param bufname string of terminal
+---@return string
+local function insert_new_term_config(bufname)
   terms[bufname] = {
     bufname = bufname,
     jobid = -1,
     bufid = -1,
-    terminal_opened_win_id = -1,
+    winid = -1,
+    before_wind_id = -1,
   }
+  return bufname
  end
 
 --- Show terminal for id
----@param num number
+---@param key_term string
 ---@param wind_id number
-local function showTerm(num, wind_id)
-  local index = getIndex(num)
-  terms[index].terminal_opened_win_id = wind_id
-  vim.cmd(options.buffer_pos .. "| buffer " .. terms[index].bufname)
+local function show_term(key_term, wind_id)
+  terms[key_term].before_wind_id = wind_id
+  vim.cmd(options.buffer_pos .. "| buffer " .. terms[key_term].bufname)
+  vim.wo.scl='no'
+  terms[key_term].winid = vim.fn.win_getid()
   vim.cmd("startinsert")
 end
 
 --- Create new terminal
----@param num number
+---@param key_term string
 ---@param wind_id number
-local function newTerm(num, wind_id)
-  local index = getIndex(num)
-  terms[index].terminal_opened_win_id = wind_id
+local function create_new_term(key_term, wind_id)
+  terms[key_term].before_wind_id = wind_id
   vim.cmd(options.buffer_pos .. "| term")
   vim.bo.ft = "better_term"
-  vim.cmd("file " .. terms[index].bufname)
+  vim.cmd("file " .. terms[key_term].bufname)
   vim.wo.relativenumber = false
   vim.o.number = false
   vim.bo.buflisted = false
   vim.wo.foldcolumn = '0'
   vim.bo.readonly = true
-  terms[index].bufid = vim.api.nvim_buf_get_number(0)
-  terms[index].jobid = vim.b.terminal_job_id
+  vim.wo.scl='no'
+  terms[key_term].bufid = vim.api.nvim_buf_get_number(0)
+  terms[key_term].jobid = vim.b.terminal_job_id
+  terms[key_term].winid = vim.fn.win_getid()
   vim.cmd("startinsert")
 end
 
-local function getLastTerm()
+--- get term id
+---@return boolean
+local function is_cbuffer_term()
   local name = vim.fn.bufname("%")
   if name:find('^' .. options.prefix) ~= nil then
-    local index = name:gsub(options.prefix, "")
-    return tonumber(index)
+    return true
   end
-  return 0
+  return false
 end
 
 --- Hide current Term
-local function hideLastTerm(num)
-  local last = getLastTerm()
-  if last == 0 then
-    return
-  elseif last ~= num then
+local function hide_current_term_on_win()
+  if is_cbuffer_term() then
     vim.cmd(":hide")
+    return
+  end
+  local all_terms = vim.tbl_keys(terms)
+  for _, term_name in pairs(all_terms) do
+    if vim.fn.bufwinnr(term_name) > 0 then
+      vim.fn.win_gotoid(terms[term_name].winid)
+      vim.cmd(":hide")
+      return
+    end
   end
 end
 
-local function validateTerm(num)
-  num = num or 1
-  if vim.tbl_isempty(terms) then
-    newTermcConfig(num)
-    return num
-  elseif not terms[getIndex(num)] then
-    newTermcConfig(num)
-    return num
+--- Validate if exist the terminal
+---@param index string? | number?
+---@return string
+local function create_term_key(index)
+  local default = options.prefix .. "1"
+  if type(index) == "number" then
+    index = get_term_key(index) or default
+  else
+  index = index or default
   end
-  return num
+  if vim.tbl_isempty(terms) then
+    return insert_new_term_config(index)
+  elseif not terms[index] then
+    return insert_new_term_config(index)
+  end
+  return index
 end
 
 --- Show or hide Term
----@param num number
-function M.open(num)
-  num = validateTerm(num)
-  local index = getIndex(num)
+---@param index string? | number?
+function M.open(index)
+  index = create_term_key(index)
   local buf_exist = vim.api.nvim_buf_is_valid(terms[index].bufid)
   local current_wind_id = vim.api.nvim_get_current_win()
   if buf_exist then
     local bufinfo = vim.fn.getbufinfo(terms[index].bufid)[1]
     if bufinfo.hidden == 1 then
-      hideLastTerm(num)
-      showTerm(num, current_wind_id)
+      hide_current_term_on_win()
+      show_term(index, current_wind_id)
     else
       vim.fn.win_gotoid(bufinfo.windows[1])
       vim.cmd(":hide")
-      if current_wind_id ~= terms[index].terminal_opened_win_id and current_wind_id ~= bufinfo.windows[1] then
+      if current_wind_id ~= terms[index].before_wind_id and current_wind_id ~= bufinfo.windows[1] then
         vim.fn.win_gotoid(current_wind_id)
-        hideLastTerm(num)
-        showTerm(num, current_wind_id)
+        hide_current_term_on_win()
+        show_term(index, current_wind_id)
       end
     end
   else
-    hideLastTerm(num)
-    newTerm(num, current_wind_id)
+    hide_current_term_on_win()
+    create_new_term(index, current_wind_id)
   end
 end
 
@@ -135,25 +155,23 @@ function M.send(cmd, num, interrupt)
     return
   end
   interrupt = interrupt or false
-  local index = getIndex(num)
-  local buf_exist = vim.api.nvim_buf_is_valid(terms[index].bufid)
+  local key_term = get_term_key(num)
+  local buf_exist = vim.api.nvim_buf_is_valid(terms[key_term].bufid)
   if buf_exist then
     if interrupt then
-      M.open(num)
-      vim.api.nvim_chan_send(terms[index].jobid, vim.api.nvim_replace_termcodes('<C-c> <C-l>', true, true, true))
+      vim.api.nvim_chan_send(terms[key_term].jobid, vim.api.nvim_replace_termcodes('<C-c> <C-l>', true, true, true))
       vim.loop.sleep(100)
     end
-    vim.api.nvim_chan_send(terms[index].jobid, cmd .. "\n")
+    vim.api.nvim_chan_send(terms[key_term].jobid, cmd .. "\n")
   else
-    M.open(num)
-    vim.api.nvim_chan_send(terms[index].jobid, cmd .. "\n")
+    vim.api.nvim_chan_send(terms[key_term].jobid, cmd .. "\n")
   end
 end
 
 --- Select term and show or hide
 function M.select()
   if vim.tbl_isempty(terms) then
-    print("Empy betterTerms")
+    print("Empty betterTerm's")
     return
   end
   vim.ui.select(vim.tbl_keys(terms), {
@@ -163,8 +181,7 @@ function M.select()
     end,
   }, function(term, _)
     if term then
-      term = term:gsub(options.prefix, "")
-      M.open(tonumber(term))
+      M.open(term)
     else
       print("Term not valid")
     end
